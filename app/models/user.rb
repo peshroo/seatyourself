3bd689b9d3b0980e04ba3bed762b3e7ed835a93c
Doug <doug.steinberg@gmail.com>
2017-06-18 14:28:26 -0400

    Resolved conflict in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-   has_many :reservations
-   has_one :restaurant
-=======
-  has_secure_password 
->>>>>>> 7aab826afa876285a0400980fd6766ece76788e6
+  has_secure_password
+
+  has_many :reservations
+  has_one :restaurant
+
 end
