808e4581e3aded3a4db288b4f15003c2ef01f7df
Doug <doug.steinberg@gmail.com>
2017-06-18 15:00:53 -0400

    Added link to users in home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.phone_number = params[:user][:phone_number]
-    @user.party_size = params[:user][:party_size]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
+    # @user.name = params[:user][:name]
+    # @user.email = params[:user][:email]
+    # @user.phone_number = params[:user][:phone_number]
+    # @user.party_size = params[:user][:party_size]
+    # @user.password = params[:user][:password]
+    # @user.password_confirmation = params[:user][:password_confirmation]
     if @user.save
       flash[:notice] = "User succesfully created!"
       redirect_to "/users"
