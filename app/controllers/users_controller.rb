e5f2f2e0c3d8f080a74388a69e76dfedf14ef9af
Doug <doug.steinberg@gmail.com>
2017-06-18 14:37:22 -0400

    Create method in user controller, user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,30 @@ class UsersController < ApplicationController
 
   def new
       @user = User.new
-    end
-
-    def create
-      @user = User.new(user_params)
-      @user.password = params[:user][:password]
-      @user.password_confirmation = params[:user][:password_confirmation]
-      if @user.save
-        redirect_to "/users"
-    end
-
   end
 
-    private
-
-    def user_params
-      params.require(:user).permit(:email, :password,
-      :password_confirmation)
+  def create
+    @user = User.new(user_params)
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.phone_number = params[:user][:phone_number]
+    @user.party_size = params[:user][:party_size]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.save
+      flash[:notice] = "User succesfully created!"
+      redirect_to "/users"
+    else
+      flash[:notice] = "User unable to be created. Please try again!"
+      render :new
     end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:email, :password,
+    :password_confirmation)
+  end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'restaurants#index'
   resources :restaurants
+
+  resource :users, only: [:new, :create]
 end
