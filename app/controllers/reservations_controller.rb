f0b712543252fe6c60b917452e255957546cc066
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-19 15:05:11 -0400

    filling in reservations controller and working on reservation views

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,16 @@
 class ReservationsController < ApplicationController
+
   def index
     @reservations = Reservation.all
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619181908) do
+ActiveRecord::Schema.define(version: 20170619182830) do
 
   create_table "reservations", force: :cascade do |t|
     t.datetime "address"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170619181908) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "party_size"
+    t.datetime "date_time"
     t.index ["restaurant_id"], name: "index_reservations_on_restaurant_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
