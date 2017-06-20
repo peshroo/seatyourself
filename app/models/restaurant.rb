5adf884d6ed46ac97330ac611cd631e6bbabde6d
Doug <doug.steinberg@gmail.com>
2017-06-20 16:58:49 -0400

    Added open/close method in restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,8 @@ class Restaurant < ApplicationRecord
   has_many :reservations
 
   CAPACITY = 20
+  OPENS = 11   #11am
+  CLOSES = 23  #11pm
 
   def capacity_remaining(date, time)     #only matters for given date and time. (one hours blocks??)
     total = 0
@@ -11,11 +13,13 @@ class Restaurant < ApplicationRecord
     CAPACITY - total
   end
 
-  # def open?(time)
-  #   if time > opens && time < closes
-  #     return true
-  #   else
-  #     errors.add(:restaurant_id, 'Sorry, the restaurant is closed at that time')
-  
+  def open_closed(time)
+    if time > OPENS && time < CLOSES
+      true
+    else
+      false
+    end
+  end
+
 
 end
